d8a026a51cb8f44df4df2310dafee206992165db
inonchiu <inonchiu@usm.lmu.de>
2016-05-31 15:19:09 +0200

    Add setup.py
    add setup.py. Now it can be installed via pip:
    
    pip install git+git://github.com/inonchiu/NavarroFrenkWhite.git

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='NavarroFrenkWhite',
+    version='1.0',
+    packages=['.'],
+      package_data={
+      '.': ['NFW'],
+      },
+    url='',
+    license='',
+    author='I-Non Chiu',
+    author_email='inonchiu@usm.lmu.de',
+    description='NFW model module.'
+)
